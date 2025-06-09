31aa8a57b2ae54ecc7146c30e00197ee5856ea76
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-06-09 20:00:57 +0200

    Final tweaks before 0.1.5

--- kubejs/server_scripts/player_login.js
+++ kubejs/server_scripts/player_login.js
@@ -18,9 +18,6 @@ ServerEvents.tick((event) => {
       server.runCommandSilent("kill @e[type=item]");
       server.tell("§7Items have been cleaned.");
       server.runCommandSilent(
-        "effect give @a farmersdelight:nourishment 180 0 true"
-      );
-      server.runCommandSilent(
         "effect give @a legendarysurvivaloverhaul:cold_resistance 280 0 true"
       );
       server.runCommandSilent(
